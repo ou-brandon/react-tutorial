d5744679fa96f95b3d83a122ccc7e984a30cbae2
ou-brandon <73049480+ou-brandon@users.noreply.github.com>
2022-04-05 22:40:02 -0400

    Removed unneeded files

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-'use strict';
-
-const e = React.createElement;
-
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
